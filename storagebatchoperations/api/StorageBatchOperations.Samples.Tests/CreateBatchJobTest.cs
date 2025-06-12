148b72a499abf5c3045e2be3b13f9ae3d43a8131
mahendra-google <mahendraw@xwf.google.com>
2025-06-12 18:03:53 +0530

    remove unused namespace

--- storagebatchoperations/api/StorageBatchOperations.Samples.Tests/CreateBatchJobTest.cs
+++ storagebatchoperations/api/StorageBatchOperations.Samples.Tests/CreateBatchJobTest.cs
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and 
 // limitations under the License.
 
-using Google.Cloud.Storage.V1;
 using Google.Cloud.StorageBatchOperations.V1;
 using System;
 using System.IO;
